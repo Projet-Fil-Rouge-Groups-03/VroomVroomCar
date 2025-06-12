02f4447a7b238c4d0f14b77071e8c6a0fc3a96e9
Emikoya <pruvost.lily@gmail.com>
2025-06-12 13:29:00 +0200

    init Trip Request DTO

--- src/main/java/fr/diginamic/VroomVroomCar/dto/request/TripRequestDto.java
+++ src/main/java/fr/diginamic/VroomVroomCar/dto/request/TripRequestDto.java
@@ -1,4 +1,47 @@
 package fr.diginamic.VroomVroomCar.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
 public class TripRequestDto {
+
+    private Integer id;
+
+    @NotNull
+    private Date dateDebut;
+    @NotNull
+    private Date dateFin;
+
+    @NotNull
+    private LocalTime heureDepart;
+    private LocalTime heureArrivee;
+
+    @NotBlank
+    private String lieuDepart;
+    @NotBlank
+    private String lieuArrivee;
+
+    @NotBlank
+    private String villeDepart;
+    @NotBlank
+    private String villeArrivee;
+
+    @Min(0)
+    private int nbPlacesRestantes;
+
+    @NotNull
+    private Integer organisateurId;
+
+    @NotNull
+    private Integer carId;
+
+
 }
