8323bac3f1a0f2bf79ac1a184ec3698db151832c
Emikoya <pruvost.lily@gmail.com>
2025-06-12 14:23:12 +0200

    Ajout méthode CRUD du Repository + javadoc

--- src/main/java/fr/diginamic/VroomVroomCar/repository/TripRepository.java
+++ src/main/java/fr/diginamic/VroomVroomCar/repository/TripRepository.java
@@ -4,9 +4,26 @@ import fr.diginamic.VroomVroomCar.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository JPA pour l'entité Trip.
+ */
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Integer> {
-    Optional<Trip> findById(int id);
+    @Override
+    Trip save(Trip trip);
+
+    @Override
+    List<Trip> findAll();
+
+    @Override
+    Optional<Trip> findById(Integer id);
+
+    @Override
+    void deleteById(Integer id);
+
+    @Override
+    boolean existsById(Integer id);
 }
