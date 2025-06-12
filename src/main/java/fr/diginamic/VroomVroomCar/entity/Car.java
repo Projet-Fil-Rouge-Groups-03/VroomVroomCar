bc0a23a1a5e10c3d8cb3a050a4376e94be798aea
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-12 14:23:37 +0200

    Update import car

--- src/main/java/fr/diginamic/VroomVroomCar/entity/Car.java
+++ src/main/java/fr/diginamic/VroomVroomCar/entity/Car.java
@@ -11,14 +11,6 @@ import lombok.NoArgsConstructor;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Cette classe représente une entité Voiture dans la base de données.
  * Elle contient les informations relatives à une voiture, telles que la marque,
