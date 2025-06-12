fadd146e08e069a581c14e017ec85b6748a46794
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-12 11:49:20 +0200

    Create mysql + SwaggerConfig

--- src/main/java/fr/diginamic/VroomVroomCar/config/SwaggerConfig.java
+++ src/main/java/fr/diginamic/VroomVroomCar/config/SwaggerConfig.java
@@ -1,4 +1,20 @@
 package fr.diginamic.VroomVroomCar.config;
 
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API VroomVroomCar")
+                        .version("1.0")
+                        .description("Cette API fournit des données pour le site VroomVroomCar.")
+                        .contact(new Contact().name("VroomVroomCar Corp.")));
+    }
 }
