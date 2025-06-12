3c20ef700eebd5c3f6e4c1203d42b1abb808b6bb
Manax166 <paul.ripault@gmail.com>
2025-06-12 15:33:17 +0200

    Ajout des traitements pour les get et le post sur UserController, UserService, UserRepository
    # Conflicts:
    #	src/main/java/fr/diginamic/VroomVroomCar/repository/UserRepository.java

--- src/main/java/fr/diginamic/VroomVroomCar/repository/UserRepository.java
+++ src/main/java/fr/diginamic/VroomVroomCar/repository/UserRepository.java
@@ -2,6 +2,9 @@ package fr.diginamic.VroomVroomCar.repository;
 
 import fr.diginamic.VroomVroomCar.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByNom(String nom);
 }
