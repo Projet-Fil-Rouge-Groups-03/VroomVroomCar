a782fd0e2e5e5ce8eae91800a0ec4880a5a5fc3e
Emikoya <pruvost.lily@gmail.com>
2025-06-12 13:31:30 +0200

    init Trip Response DTO

--- src/main/java/fr/diginamic/VroomVroomCar/dto/response/TripResponseDto.java
+++ src/main/java/fr/diginamic/VroomVroomCar/dto/response/TripResponseDto.java
@@ -1,4 +1,45 @@
 package fr.diginamic.VroomVroomCar.dto.response;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
 public class TripResponseDto {
+
+        private Integer id;
+
+        @NotNull
+        private Date dateDebut;
+        @NotNull
+        private Date dateFin;
+
+        @NotNull
+        private LocalTime heureDepart;
+        private LocalTime heureArrivee;
+
+        @NotBlank
+        private String lieuDepart;
+        @NotBlank
+        private String lieuArrivee;
+
+        @NotBlank
+        private String villeDepart;
+        @NotBlank
+        private String villeArrivee;
+
+        @Min(0)
+        private int nbPlacesRestantes;
+
+        @NotNull
+        private Integer organisateurId;
+
+        @NotNull
+        private Integer carId;
 }
