1360582f078b6ec2135b7a93bf4e3e23c8280251
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-12 12:16:18 +0200

    Update Entity

--- src/main/java/fr/diginamic/VroomVroomCar/entity/Car.java
+++ src/main/java/fr/diginamic/VroomVroomCar/entity/Car.java
@@ -1,10 +1,15 @@
 package fr.diginamic.VroomVroomCar.entity;
 
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -17,24 +22,37 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "La marque est obligatoire")
+    @Column(nullable = false, length = 30)
     private String marque;
-    private String modele;
-    private int nb_de_places;
-    private String pollution;
-    private String type;
-    private String infos_supp;
 
-    @Enumerated(EnumType.STRING)
-    private Categorie categorie;
+    @NotBlank(message = "Le modèle est obligatoire")
+    @Column(nullable = false, length = 30)
+    private String modele;
+
+    @Min(value = 1)
+    @Max(value = 10)
+    @Column(name = "nb_places", nullable = false)
+    private int nbDePlaces;
+
+    @Column(length = 30)
+    private String pollution;
+
+    @Lob
+    @Column(name = "infos_supp")
+    private String infosSupp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "utilisateurId", nullable = false)
+    private User user;
 
     @Enumerated(EnumType.STRING)
     private Motorisation motorisation;
 
-    @OneToMany
-    private Trip trip;
+    @Enumerated(EnumType.STRING)
+    private Categorie categorie;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
 
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Trip> trip = new HashSet<>();
 }
