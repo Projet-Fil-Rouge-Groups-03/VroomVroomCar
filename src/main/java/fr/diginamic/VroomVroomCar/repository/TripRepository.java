4378d200c50c31e24a32ab867fc047153a62f03a
Emikoya <pruvost.lily@gmail.com>
2025-06-12 13:58:02 +0200

    Init TripRepository

--- src/main/java/fr/diginamic/VroomVroomCar/repository/TripRepository.java
+++ src/main/java/fr/diginamic/VroomVroomCar/repository/TripRepository.java
@@ -1,4 +1,12 @@
 package fr.diginamic.VroomVroomCar.repository;
 
-public class TripRepository {
+import fr.diginamic.VroomVroomCar.entity.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Integer> {
+    Optional<Trip> findById(int id);
 }
