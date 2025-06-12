524bdb9bbcece372c4337b0952ac79793bbce6b8
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-12 10:09:25 +0200

    initial commit

---
+++ src/main/java/fr/diginamic/VroomVroomCar/VroomVroomCarApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.VroomVroomCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomVroomCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroomVroomCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/VroomVroomCar/VroomVroomCarApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.VroomVroomCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomVroomCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
