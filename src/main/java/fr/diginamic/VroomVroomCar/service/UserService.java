0f747a3ad42a8c65f96472a4687bcb5a3adf73fd
Manax166 <paul.ripault@gmail.com>
2025-06-13 12:28:52 +0200

    ajout mapper, userRequestDTO, userResultDTO, IUserService et modifications de UserRepository et de UserService
    # Conflicts:
    #	src/main/java/fr/diginamic/VroomVroomCar/util/ValidationUtil.java

--- src/main/java/fr/diginamic/VroomVroomCar/service/UserService.java
+++ src/main/java/fr/diginamic/VroomVroomCar/service/UserService.java
@@ -1,46 +1,100 @@
 package fr.diginamic.VroomVroomCar.service;
 
+import fr.diginamic.VroomVroomCar.dto.request.UserRequestDto;
+import fr.diginamic.VroomVroomCar.dto.response.UserResponseDto;
 import fr.diginamic.VroomVroomCar.entity.Status;
 import fr.diginamic.VroomVroomCar.entity.User;
 import fr.diginamic.VroomVroomCar.exception.ResourceNotFoundException;
+import fr.diginamic.VroomVroomCar.mapper.UserMapper;
 import fr.diginamic.VroomVroomCar.repository.UserRepository;
+import fr.diginamic.VroomVroomCar.util.ValidationUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class UserService {
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService implements IUserService {
+
+    @Autowired
+    UserMapper userMapper;
 
     @Autowired
     UserRepository userRepository;
-    public List<User> findAll() {
-        return userRepository.findAll();
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UserResponseDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toResponseDto)
+                .collect(Collectors.toList());
+    }
+    @Transactional(readOnly = true)
+    @Override
+    public UserResponseDto getUserById(Integer id) throws ResourceNotFoundException {
+        ValidationUtil.validateIdNotNull(id, "l'utilisateur'");
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Utilisateur non trouvé avec l'ID: " + id));
+
+        return userMapper.toResponseDto(user);
+    }
+    @Transactional(readOnly = true)
+    @Override
+    public UserResponseDto getByNom(String nom) throws ResourceNotFoundException {
+        ValidationUtil.validateNotNull(nom);
+        User user = userRepository.findByNom(nom)
+                .orElseThrow(() -> new ResourceNotFoundException("Utilisateur non trouvé avec le nom: " + nom));
+        return userMapper.toResponseDto(user);
     }
 
-    public User findById(int id){
-        return userRepository.findById(id)
-                .get();
+    @Override
+    public UserResponseDto createUser(UserRequestDto userRequestDto) throws ResourceNotFoundException {
+        ValidationUtil.validateUserRequestDto(userRequestDto);
+
+        User user = userMapper.toEntity(userRequestDto, Status.ACTIF);
+        return userMapper.toResponseDto(userRepository.save(user));
     }
 
-    public User findByNom(String nom){
-        return userRepository.findByNom(nom);
+    // UPDATE
+
+    @Override
+    public UserResponseDto updateUser(Integer id, UserRequestDto userRequestDto) throws ResourceNotFoundException{
+        ValidationUtil.validateIdNotNull(id, "l'utilisateur'");
+        ValidationUtil.validateUserRequestDto(userRequestDto);
+
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Utilisateur non trouvée avec l'ID: " + id));
+
+        userMapper.updateEntity(existingUser, userRequestDto);
+        return userMapper.toResponseDto(userRepository.save(existingUser));
     }
 
-    public void addUser(User user){
-        userRepository.save(new User(
-                user.getNom(),
-                user.getPrenom(),
-                user.getMail(),
-                user.getAdresse(),
-                user.getMotDePasse(),
-                Status.ACTIF));
+    @Override
+    public UserResponseDto updateUser(String nom, UserRequestDto userRequestDto) throws ResourceNotFoundException{
+        ValidationUtil.validateNotNull(nom);
+        ValidationUtil.validateUserRequestDto(userRequestDto);
+
+        User existingUser = userRepository.findByNom(nom)
+                .orElseThrow(() -> new ResourceNotFoundException("Utilisateur non trouvée avec l'ID: " + nom));
+
+        userMapper.updateEntity(existingUser, userRequestDto);
+        return userMapper.toResponseDto(userRepository.save(existingUser));
     }
 
-    public void editUser(int id, User user){
-    }
+    // DELETE
 
-    public void editUser(String nom, User user){
-
+    @Override
+    public void deleteUser(Integer id) throws ResourceNotFoundException {
+        ValidationUtil.validateIdNotNull(id, "l'utilisateur");
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Utilisateur non trouvée avec l'ID: " + id);
+        }
+        userRepository.deleteById(id);
     }
 
 }
