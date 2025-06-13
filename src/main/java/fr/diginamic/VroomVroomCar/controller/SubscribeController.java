a0554b2613dcdbe09e9abd132c1eb5764ff4034e
Manax166 <paul.ripault@gmail.com>
2025-06-13 16:49:42 +0200

    Ajout du controller et début des méthodes

--- src/main/java/fr/diginamic/VroomVroomCar/controller/SubscribeController.java
+++ src/main/java/fr/diginamic/VroomVroomCar/controller/SubscribeController.java
@@ -1,4 +1,44 @@
 package fr.diginamic.VroomVroomCar.controller;
 
-public class SubscribeController {
+import fr.diginamic.VroomVroomCar.dto.request.SubscribeRequestDto;
+import fr.diginamic.VroomVroomCar.dto.response.SubscribeResponseDto;
+import fr.diginamic.VroomVroomCar.exception.ResourceNotFoundException;
+import fr.diginamic.VroomVroomCar.service.SubscribeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subscribe")
+public class SubscribeController implements ISubscribeController{
+    @Autowired
+    SubscribeService subscribeService;
+
+    @Override
+    public ResponseEntity<List<SubscribeResponseDto>> findAll() {
+        return ResponseEntity.ok(subscribeService.getAllSubscribes());
+    }
+
+    @Override
+    public ResponseEntity<SubscribeResponseDto> findById(int id) throws ResourceNotFoundException {
+        return ResponseEntity.ok(subscribeService.getSubscribeById);
+    }
+
+    @Override
+    public ResponseEntity<SubscribeResponseDto> addSubscribe(SubscribeRequestDto subscribe) throws ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SubscribeResponseDto> editSubscribe(int id, SubscribeRequestDto subscribe) throws ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> deleteSubscribe(int id) throws ResourceNotFoundException {
+        return null;
+    }
 }
--- src/main/java/fr/diginamic/VroomVroomCar/service/SubscribeService.java
+++ src/main/java/fr/diginamic/VroomVroomCar/service/SubscribeService.java
@@ -1,4 +1,17 @@
 package fr.diginamic.VroomVroomCar.service;
 
+import fr.diginamic.VroomVroomCar.dto.response.SubscribeResponseDto;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
 public class SubscribeService {
+
+    public @Nullable SubscribeResponseDto getSubscribeById(){
+        return null;
+    }
+
+    public @Nullable List<SubscribeResponseDto> getAllSubscribes() {
+        return null;
+    }
 }
