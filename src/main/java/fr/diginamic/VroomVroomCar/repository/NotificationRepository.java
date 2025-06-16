06b6ba27a80d117e1f0dff15b1ba36325e1739c5
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-16 12:04:36 +0200

    Create Notification repo

--- src/main/java/fr/diginamic/VroomVroomCar/repository/NotificationRepository.java
+++ src/main/java/fr/diginamic/VroomVroomCar/repository/NotificationRepository.java
@@ -1,4 +1,20 @@
 package fr.diginamic.VroomVroomCar.repository;
 
-public class NotificationRepository {
+import fr.diginamic.VroomVroomCar.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Pageable;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    /**
+     * Trouve toutes les notifications d'un utilisateur par son ID, triées par date décroissante
+     *
+     * @param userId L'identifiant de l'utilisateur dont on veut trouver les notifications.
+     * @param pageable Les informations de pagination et de tri.
+     * @return Une page de notifications appartenant à l'utilisateur.
+     */
+    Page<Notification> findByUserIdOrderByDateDesc(Integer userId, Pageable pageable);
+
 }
