336d5a1886c7bf78eee9c591ffcd98847605a15b
Emikoya <pruvost.lily@gmail.com>
2025-06-16 14:53:55 +0200

    Mise en place de la config de sécurité

--- src/main/java/fr/diginamic/VroomVroomCar/config/SecurityConfig.java
+++ src/main/java/fr/diginamic/VroomVroomCar/config/SecurityConfig.java
@@ -1,4 +1,38 @@
 package fr.diginamic.VroomVroomCar.config;
 
+import fr.diginamic.VroomVroomCar.security.JwtFilter;
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/register", "/logout").permitAll()
+                        .requestMatchers("/api/**").hasAnyRole("ACTIF", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // utile pour afficher h2-console
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+
+                .addFilterBefore((Filter) jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
