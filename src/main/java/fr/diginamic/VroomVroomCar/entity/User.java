f376ea692ca27f91aa02d09ca9fbc0e67ed1a310
Emikoya <pruvost.lily@gmail.com>
2025-06-17 15:49:49 +0200

    Injection des FakeData + corrections

--- src/main/java/fr/diginamic/VroomVroomCar/entity/User.java
+++ src/main/java/fr/diginamic/VroomVroomCar/entity/User.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @Table(name = "utilisateur")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nom;
     private String prenom;
