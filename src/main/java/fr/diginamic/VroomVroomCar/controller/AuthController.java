5264e0a4422441d87e15bae0338f5c7f670f5083
Manax166 <paul.ripault@gmail.com>
2025-06-17 12:13:41 +0200

    Ajout AuthController login
    # Conflicts:
    #	pom.xml

--- src/main/java/fr/diginamic/VroomVroomCar/controller/AuthController.java
+++ src/main/java/fr/diginamic/VroomVroomCar/controller/AuthController.java
@@ -1,4 +1,26 @@
 package fr.diginamic.VroomVroomCar.controller;
 
+import fr.diginamic.VroomVroomCar.entity.User;
+import fr.diginamic.VroomVroomCar.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) throws Exception {
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, authService.logUser(user).toString())
+                .body("vous êtes login");
+    }
 }
