68b4867bc0865c9cc01668e9bdff27a6ab31bfc6
Manax166 <paul.ripault@gmail.com>
2025-06-18 16:53:59 +0200

    Modification de AuthService et ajout de findByMail

--- src/main/java/fr/diginamic/VroomVroomCar/repository/UserRepository.java
+++ src/main/java/fr/diginamic/VroomVroomCar/repository/UserRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     Optional<User> findByNom(String nom);
+    Optional<User> findByMail(String email);
 }
