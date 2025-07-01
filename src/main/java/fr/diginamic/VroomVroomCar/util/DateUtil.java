554ac74b8a1b4bc9f50ce8bded1aac135d0ba6e7
Emikoya <pruvost.lily@gmail.com>
2025-07-01 15:21:18 +0200

    Correction route Search CompanyCar + convert string to sql Date

--- src/main/java/fr/diginamic/VroomVroomCar/util/DateUtil.java
+++ src/main/java/fr/diginamic/VroomVroomCar/util/DateUtil.java
@@ -1,5 +1,6 @@
 package fr.diginamic.VroomVroomCar.util;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -18,5 +19,20 @@ public class DateUtil {
     public static String formatToFrench(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return date.format(formatter);
+    }
+
+    public static Date convertStringToDate(String dateStr){
+        Date dateConverted = null ;
+        if (dateStr != null && !dateStr.trim().isEmpty()) {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate localDateFin = LocalDate.parse(dateStr, formatter);
+                dateConverted = Date.valueOf(localDateFin);
+            } catch (java.time.format.DateTimeParseException e) {
+                System.err.println("Erreur de format de date pour dateFin : " + dateStr + " - " + e.getMessage());
+                throw new IllegalArgumentException("Format de date invalide pour dateFin. Utilisez le format AAAA-MM-JJ.", e);
+            }
+        }
+        return dateConverted;
     }
 }
