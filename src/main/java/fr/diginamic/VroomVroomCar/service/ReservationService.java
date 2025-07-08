0cb3b87a1c7db0c053aa68b9b2e70b5437fb0147
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-08 17:21:49 +0200

    Clean up

--- src/main/java/fr/diginamic/VroomVroomCar/service/ReservationService.java
+++ src/main/java/fr/diginamic/VroomVroomCar/service/ReservationService.java
@@ -90,12 +90,9 @@ public class ReservationService implements IReservationService {
     }
 
     public List<ReservationResponseDto> getReservationsByUserId(Integer userId) {
-        // On demande au repository de trouver toutes les réservations pour cet utilisateur.
         List<Reservation> reservations = reservationRepository.findByUserId(userId);
-
-        // On transforme la liste d'entités en une liste de DTOs pour la réponse.
         return reservations.stream()
-                .map(reservationMapper::toResponse) // Utilise ton mapper
+                .map(reservationMapper::toResponse)
                 .collect(Collectors.toList());
     }
 
