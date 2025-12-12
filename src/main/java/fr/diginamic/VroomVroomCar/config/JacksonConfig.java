f0a70c63d142b760b8ea6b839f9e54cc48d32dc7
Emikoya <pruvost.lily@gmail.com>
2025-12-12 11:40:22 +0100

    change JacksonConfig

--- src/main/java/fr/diginamic/VroomVroomCar/config/JacksonConfig.java
+++ src/main/java/fr/diginamic/VroomVroomCar/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package fr.diginamic.VroomVroomCar.config;
 
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,6 +10,8 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return JsonMapper.builder()
+                .findAndAddModules()
+                .build();
     }
 }
